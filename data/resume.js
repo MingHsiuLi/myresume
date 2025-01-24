37ee9a34d62cd38cfe6a09d2b0fb91bb3a3e0b03
MingHsiuLi <li4286@gmail.com>
2025-01-24 12:43:07 +0800

    initial upload

---
+++ data/resume.js
@@ -0,0 +1,137 @@
+const resumeData = 
+{
+    "personal": {
+        "name": "李明修",
+        "picture": "ming.jpg",
+        "gender": "男",
+        "birthday": "1970-09",
+        "age": 54,
+        "address": "新北市三重區",
+        "phone": "0939-361-575",
+        "email": "li4286@gmail.com",
+        "married": true,
+        "about": " ",
+        "interests": " ",
+        "objective": "研發經理/專案經理/產品經理",
+        "aboutme": "擁有豐富的學術背景和多年的工作經驗，對機械工程和研發領域具有廣泛的知識、經驗和技能，同時帶領專案團隊或研發團隊也有15年以上經驗。擁有流利的英文能力，尤其是跨國合作的協同開發特別有經驗。對於新的領域非常有興趣，像是電動車、機器人、智慧機器、智慧物聯網等。雖然年紀已50+，還是期待可以嘗試新的領域以取得更多的成就，為公司的發展做出更大的貢獻。"
+    },
+
+    "education": [
+        {
+            "school": "德州農工大學",
+            "department": "機械所",
+            "time": "2000-2004",
+            "degree": "博士",
+            "thesis": "論文:磁浮軸承容錯控制"
+        },
+        {
+            "school": "成功大學",
+            "department": "航太所",
+            "time": "1994-1996",
+            "degree": "碩士",
+            "thesis": "論文:有限條元法"
+        },
+        {
+            "school": "中興大學",
+            "department": "農機系",
+            "time": "1990-1994",
+            "degree": "學士",
+            "thesis": ""
+        }
+    ],
+
+    "experience": [
+        {
+            "company": "朝程工業",
+            "location": "台中市南屯區",
+            "field": "產業: 手持式電動工具",
+            "department": "研發部",
+            "title": "經理",
+            "time": "2018/12-2024/07",
+            "duration": "5年8個月",
+            "description": [
+                "管理研發團隊（約20+人，含3個ME團隊、ID及DCC)：招募、訓練、輔導、KPI設定和追蹤等",
+                "協同PM團隊管理工具專案（衝擊扳手、棘輪扳手、電鑽、黃油槍、矽膠槍、打釘槍、打臘機等）",
+                "團隊每年開發約15個新產品和3~5項發明專利，新產品貢獻約20%的營收(次年)",
+                "協同銷售團隊規劃1~3年產品RoadMap",
+                "執行逢甲大學產學合作:MSC Adams在手持式電動工具之運用",
+                "協助執行工研院合作案:智慧精密起子工具開發"
+            ]
+        },
+        {
+            "company": "Stanley Black & Decker - 偉全",
+            "location": "台中市大里區",
+            "field": "產業: 手持式氣動工具",
+            "department": "研發部",
+            "title": "資深專案工程師",
+            "time": "2011/03-2016/06",
+            "duration": "5年4個月",
+            "description": [
+                "協同美國開發團隊執行氣動釘槍開發專案",
+                "協同大陸製造團隊執行氣動釘槍量產",
+                "分析與解決氣動釘槍測試問題",
+                "主持會議與追蹤問題進度",
+                "執行CAE結構強度分析"
+            ]          
+        },
+        {
+            "company": "元力電機",
+            "location": "台中市東區",
+            "field": "產業: PC散熱風扇",
+            "department": "研發部",
+            "title": "經理",
+            "time": "2007/04-2011/01",
+            "duration": "3年10個月",
+            "description": [
+                "管理研發團隊（約5~8人)",
+                "執行筆電用之離心扇(centrifugal fan/blower)和軸流扇(axial fan)之設計",
+                "執行前端CAE Fluent流體分析及優化設計",
+                "協同客戶端解決PC系統振動噪音問題",
+                "協同業務開發客戶"
+            ]
+        },
+
+        {
+            "company": "神達電腦",
+            "location": "桃園市龜山區",
+            "field": "產業: 電腦",
+            "department": "研發部",
+            "title": "資深熱傳工程師",
+            "time": "2005/11-2007/03",
+            "duration": "1年5個月",
+            "description": [
+                "執行電腦系統散熱模組之設計、分析(Icepack & Flotherm)及量測",
+                "執行電腦系統聲壓、聲功率及聲音品質之量測與分析",
+            ]
+        }, 
+        {
+            "company": "飛瑞電子",
+            "location": "新北市汐止區",
+            "field": "產業: 不斷電系統",
+            "department": "研發部",
+            "title": "資深機構工程師",
+            "time": "1998/06-1999/09",
+            "duration": "1年4個月",
+            "description": [
+                "執行不斷電系統機構設計(AuoCad)、包裝設計(PE Foam, Wood)",
+                "執行不斷電系統之振動、落下及噪音測試",
+            ]
+        }, 
+        
+    ],
+
+    "skills": [["CAD:", "Pro-E", "Solidworks", "AutoCAD"], 
+               ["CAE:", "Fluent", "IcePack", "Flotherm", "AnsysMechanical", "GearTeq"],
+               ["Programming:", "Javascript", "Python", "ArduinoSketch", "C/C++", "Php", "Matlab", "Fortran"],
+               ["Software:", "MS-Office", "MS-Project"],
+               ["Webpage:", "HTML", "CSS", "Bootstrap", "MySql", "MongoDB"],
+               ["App:", "Thunkable", "App-Inventor"],
+               ["Management:", "研發團隊管理", "專案管理", "策略規劃與制定", "問題解決", "產學合作", "異業合作","跨部門/跨國溝通"]
+    ],
+
+    "certifications": ["PMP專案管理", "TOEIC金色證書", "GEPT中高級", "網頁設計丙級", "電腦軟體應用乙級", "Engineer-in-Training Certification"]
+}
+
+
+
+
